20422ee25d7997b5bbe3d17cf6a7d44a20193cba
Conrado Carrizo <conradocarrizo@gmail.com>
2023-07-26 23:17:16 -0300

    me traiciono el control + S

--- moni/tests/test_users.py
+++ moni/tests/test_users.py
@@ -1,14 +1,13 @@
 import pytest
 from django.contrib.auth.models import User
 
+
 @pytest.mark.django_db
 def test_user_creation():
     user = User.objects.create(
-        username='testuser',
-        email='testuser@example.com',
-        password='testpassword'
+        username="testuser", email="testuser@example.com", password="testpassword"
     )
 
-    assert user.username == 'testuser'
-    assert user.email == 'testuser@example.com'
-    assert user.password == 'testpassword'
+    assert user.username == "testuser"
+    assert user.email == "testuser@example.com"
+    assert user.password == "testpassword"
